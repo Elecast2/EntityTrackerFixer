08b9ddf968c07d189cb5b2ae4e243c882a5d1db3
cascaseno <38667545+cascaseno@users.noreply.github.com>
2020-11-07 13:35:39 +0100

    Support for 1.16.4 + Error msg if /etf has 0 args.
    Added support for version 1.16.4 (1_16_R3), updated README.md and also added an error chat message if /etf command is used with 0 arguments.

--- EntityTrackerFixer/src/net/minemora/entitytrackerfixer/commands/CommandETF.java
+++ EntityTrackerFixer/src/net/minemora/entitytrackerfixer/commands/CommandETF.java
@@ -1,32 +1,31 @@
 package net.minemora.entitytrackerfixer.commands;
 
+import net.minemora.entitytrackerfixer.EntityTrackerFixer;
+import net.minemora.entitytrackerfixer.vault.VaultManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import net.minemora.entitytrackerfixer.EntityTrackerFixer;
-import net.minemora.entitytrackerfixer.vault.VaultManager;
-
 public class CommandETF implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-    	if(args.length == 0) {
-    		return true;
-    	}
-    	if(args[0].equalsIgnoreCase("reload")) {
-    		if(commandSender instanceof Player) {
-        		if (!VaultManager.hasPermission((Player) commandSender, "entitytrackerfixer.reload")) {
-                    commandSender.sendMessage(ChatColor.RED + "You don't have permission to use that command.");
-                    return true;
-                }
-        	}
-    		EntityTrackerFixer.plugin.reload();
-    		commandSender.sendMessage(ChatColor.GREEN + "The config has been reloaded sucessfully!");
-    	}
-        
-		return true;
+        if (commandSender instanceof Player) {
+            if (!VaultManager.hasPermission((Player) commandSender, "entitytrackerfixer.reload")) {
+                commandSender.sendMessage(ChatColor.RED + "You don't have permission to use that command.");
+                return true;
+            }
+        }
+        if (args.length == 0) {
+            commandSender.sendMessage(ChatColor.RED + "No arguments provided. Available: " + ChatColor.GOLD + "/etf reload" + ChatColor.RED + ".");
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            EntityTrackerFixer.plugin.reload();
+            commandSender.sendMessage(ChatColor.GREEN + "The config has been reloaded sucessfully!");
+        }
+        return true;
     }
 }
